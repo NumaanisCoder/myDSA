107255b46256d70a6f15b5883181cc509de3b0d3
NumaanisCoder <nomannaeem985@gmail.com>
2024-01-09 19:19:54 +0530

    Add Left and Right Shift Operators

--- BitwiseOperators/index.java
+++ BitwiseOperators/index.java
@@ -17,6 +17,28 @@ class index{
 
         System.out.println("XOR OF 5 and 8 : "+(5^8)); //13
 
+
+
+
+        // # Left Shift Operator
+        //It shift digit one by one to left side, effect (double the no. (X 2)) on small no. 
+        // but not on big numbers
+        //padding always 0
+
+        System.out.println("Left shift of 5 one time : "+ (5<<1)); //10
+        System.out.println("Left shift of 5 two time : "+ (5<<2)); //20
+
+        // #Right Shift Operator
+        //It shift digit one by one to left side, effect (divide the no. by two (/ 2)). 
+        // but not on big numbers
+
+        System.out.println("Right shift  of 10 one time : "+ (10>>1)); //5
+        System.out.println("Right shift  of 10 two time : "+ (10>>2)); //2
+        System.out.println("Right shift  of 7 one time : "+ (7>>1)); //3
+
+        System.out.println(-10>>3); //-2 in my case
+        //it depends upon the compiler what to add padding (0 or 1);
+
         
 
     }
