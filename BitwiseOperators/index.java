4fc734af67bbde0bdb1da2940a76c7174d76a5e5
NumaanisCoder <nomannaeem985@gmail.com>
2024-01-09 18:54:49 +0530

    Add bitwise section

---
+++ BitwiseOperators/index.java
@@ -0,0 +1,23 @@
+class index{
+    public static void main(String[] args) {
+
+//Bitwise Operators
+
+        // OR Operator( NEED ONLY ONE 1)
+        System.out.println("OR OF 12 and 7 : "+(12|7)); //15
+
+
+        //NOT Operator (Inverse 0-> 1, 1-> 0) and then represent in 2's(1's (inverse) then + 1 ) complement
+        System.out.println("NOT OF 10 : "+ ~10); // -11
+
+        //AND Operator (Need Both 1 One)
+        System.out.println("AND OF 2 and 5 : "+(2 & 5)); //0
+
+        //XOR Operator (Need Only One 1, but not two 1)
+
+        System.out.println("XOR OF 5 and 8 : "+(5^8)); //13
+
+        
+
+    }
+}
